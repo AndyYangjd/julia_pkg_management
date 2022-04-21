d6dbe417a8b36b626b4ad46df1ea8a4751d390e2
andy.yang <yjd2008@hotmail.com>
2022-04-21 20:32:35 +0800

    succeed to build and show icon

--- form/ui_julia_pkg_management.h
+++ form/ui_julia_pkg_management.h
@@ -13,6 +13,7 @@
 #include <QtCore/QVariant>
 #include <QtGui/QIcon>
 #include <QtWidgets/QApplication>
+#include <QtWidgets/QFrame>
 #include <QtWidgets/QHBoxLayout>
 #include <QtWidgets/QHeaderView>
 #include <QtWidgets/QLabel>
@@ -21,6 +22,7 @@
 #include <QtWidgets/QScrollArea>
 #include <QtWidgets/QSpacerItem>
 #include <QtWidgets/QTableView>
+#include <QtWidgets/QToolButton>
 #include <QtWidgets/QVBoxLayout>
 #include <QtWidgets/QWidget>
 
@@ -41,17 +43,19 @@ public:
     QLabel *label_julia_hub;
     QLabel *label_julia_com_en;
     QLabel *label_julia_com_cn;
+    QFrame *line_2;
     QSpacerItem *verticalSpacer;
     QHBoxLayout *horizontalLayout;
     QLabel *label_5;
     QLineEdit *lineEdit_executor;
-    QPushButton *btn_load;
+    QToolButton *btn_load;
     QSpacerItem *verticalSpacer_2;
+    QFrame *line;
     QHBoxLayout *horizontalLayout_3;
-    QPushButton *btn_add;
-    QPushButton *btn_uninstall;
-    QPushButton *btn_upgrade;
-    QPushButton *btn_upgrade_all;
+    QToolButton *btn_add;
+    QToolButton *btn_uninstall;
+    QToolButton *btn_upgrade;
+    QToolButton *btn_upgrade_all;
     QSpacerItem *horizontalSpacer_3;
     QScrollArea *scrollArea;
     QWidget *scrollAreaWidgetContents;
@@ -66,9 +70,10 @@ public:
         JuliaPkgManagement->setMinimumSize(QSize(850, 850));
         JuliaPkgManagement->setMaximumSize(QSize(850, 850));
         QIcon icon;
-        icon.addFile(QString::fromUtf8("../resource/pkg_management.svg"), QSize(), QIcon::Normal, QIcon::Off);
+        icon.addFile(QString::fromUtf8(":/image/image/pkg_management.PNG"), QSize(), QIcon::Normal, QIcon::Off);
         JuliaPkgManagement->setWindowIcon(icon);
         JuliaPkgManagement->setLayoutDirection(Qt::LeftToRight);
+        JuliaPkgManagement->setAutoFillBackground(true);
         JuliaPkgManagement->setLocale(QLocale(QLocale::English, QLocale::UnitedStates));
         verticalLayout_3 = new QVBoxLayout(JuliaPkgManagement);
         verticalLayout_3->setObjectName(QString::fromUtf8("verticalLayout_3"));
@@ -78,6 +83,7 @@ public:
         label_julia->setObjectName(QString::fromUtf8("label_julia"));
         QFont font;
         font.setPointSize(15);
+        font.setUnderline(false);
         label_julia->setFont(font);
         label_julia->setLayoutDirection(Qt::LeftToRight);
         label_julia->setTextFormat(Qt::PlainText);
@@ -121,6 +127,7 @@ public:
         label_julia_hub->setObjectName(QString::fromUtf8("label_julia_hub"));
         QFont font2;
         font2.setPointSize(10);
+        font2.setUnderline(true);
         label_julia_hub->setFont(font2);
         label_julia_hub->setAlignment(Qt::AlignCenter);
         label_julia_hub->setOpenExternalLinks(true);
@@ -151,6 +158,13 @@ public:
 
         verticalLayout_3->addLayout(horizontalLayout_2);
 
+        line_2 = new QFrame(JuliaPkgManagement);
+        line_2->setObjectName(QString::fromUtf8("line_2"));
+        line_2->setFrameShape(QFrame::HLine);
+        line_2->setFrameShadow(QFrame::Sunken);
+
+        verticalLayout_3->addWidget(line_2);
+
         verticalSpacer = new QSpacerItem(20, 13, QSizePolicy::Minimum, QSizePolicy::Expanding);
 
         verticalLayout_3->addItem(verticalSpacer);
@@ -170,9 +184,17 @@ public:
 
         horizontalLayout->addWidget(lineEdit_executor);
 
-        btn_load = new QPushButton(JuliaPkgManagement);
+        btn_load = new QToolButton(JuliaPkgManagement);
         btn_load->setObjectName(QString::fromUtf8("btn_load"));
         btn_load->setFont(font1);
+        btn_load->setAcceptDrops(false);
+        btn_load->setAutoFillBackground(false);
+        QIcon icon1;
+        icon1.addFile(QString::fromUtf8(":/image/image/upload.png"), QSize(), QIcon::Normal, QIcon::Off);
+        btn_load->setIcon(icon1);
+        btn_load->setIconSize(QSize(16, 16));
+        btn_load->setToolButtonStyle(Qt::ToolButtonIconOnly);
+        btn_load->setAutoRaise(true);
 
         horizontalLayout->addWidget(btn_load);
 
@@ -183,17 +205,30 @@ public:
 
         verticalLayout_3->addItem(verticalSpacer_2);
 
+        line = new QFrame(JuliaPkgManagement);
+        line->setObjectName(QString::fromUtf8("line"));
+        line->setFrameShape(QFrame::HLine);
+        line->setFrameShadow(QFrame::Sunken);
+
+        verticalLayout_3->addWidget(line);
+
         horizontalLayout_3 = new QHBoxLayout();
         horizontalLayout_3->setObjectName(QString::fromUtf8("horizontalLayout_3"));
-        btn_add = new QPushButton(JuliaPkgManagement);
+        btn_add = new QToolButton(JuliaPkgManagement);
         btn_add->setObjectName(QString::fromUtf8("btn_add"));
         btn_add->setMinimumSize(QSize(50, 0));
         btn_add->setMaximumSize(QSize(50, 16777215));
-        btn_add->setFont(font2);
+        QFont font3;
+        font3.setPointSize(10);
+        btn_add->setFont(font3);
+        QIcon icon2;
+        icon2.addFile(QString::fromUtf8(":/image/image/install.png"), QSize(), QIcon::Normal, QIcon::Off);
+        btn_add->setIcon(icon2);
+        btn_add->setAutoRaise(true);
 
         horizontalLayout_3->addWidget(btn_add);
 
-        btn_uninstall = new QPushButton(JuliaPkgManagement);
+        btn_uninstall = new QToolButton(JuliaPkgManagement);
         btn_uninstall->setObjectName(QString::fromUtf8("btn_uninstall"));
         QSizePolicy sizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
         sizePolicy.setHorizontalStretch(0);
@@ -202,23 +237,33 @@ public:
         btn_uninstall->setSizePolicy(sizePolicy);
         btn_uninstall->setMinimumSize(QSize(50, 0));
         btn_uninstall->setMaximumSize(QSize(50, 16777215));
-        btn_uninstall->setFont(font2);
+        btn_uninstall->setFont(font3);
+        QIcon icon3;
+        icon3.addFile(QString::fromUtf8(":/image/image/delete.png"), QSize(), QIcon::Normal, QIcon::Off);
+        btn_uninstall->setIcon(icon3);
+        btn_uninstall->setAutoRaise(true);
 
         horizontalLayout_3->addWidget(btn_uninstall);
 
-        btn_upgrade = new QPushButton(JuliaPkgManagement);
+        btn_upgrade = new QToolButton(JuliaPkgManagement);
         btn_upgrade->setObjectName(QString::fromUtf8("btn_upgrade"));
         btn_upgrade->setMinimumSize(QSize(50, 0));
         btn_upgrade->setMaximumSize(QSize(50, 16777215));
-        btn_upgrade->setFont(font2);
+        btn_upgrade->setFont(font3);
+        QIcon icon4;
+        icon4.addFile(QString::fromUtf8(":/image/image/update_one.png"), QSize(), QIcon::Normal, QIcon::Off);
+        btn_upgrade->setIcon(icon4);
+        btn_upgrade->setAutoRaise(true);
 
         horizontalLayout_3->addWidget(btn_upgrade);
 
-        btn_upgrade_all = new QPushButton(JuliaPkgManagement);
+        btn_upgrade_all = new QToolButton(JuliaPkgManagement);
         btn_upgrade_all->setObjectName(QString::fromUtf8("btn_upgrade_all"));
         btn_upgrade_all->setMinimumSize(QSize(50, 0));
         btn_upgrade_all->setMaximumSize(QSize(50, 16777215));
-        btn_upgrade_all->setFont(font2);
+        btn_upgrade_all->setFont(font3);
+        btn_upgrade_all->setIcon(icon1);
+        btn_upgrade_all->setAutoRaise(true);
 
         horizontalLayout_3->addWidget(btn_upgrade_all);
 
@@ -226,6 +271,11 @@ public:
 
         horizontalLayout_3->addItem(horizontalSpacer_3);
 
+        horizontalLayout_3->setStretch(0, 1);
+        horizontalLayout_3->setStretch(1, 1);
+        horizontalLayout_3->setStretch(2, 1);
+        horizontalLayout_3->setStretch(3, 1);
+        horizontalLayout_3->setStretch(4, 50);
 
         verticalLayout_3->addLayout(horizontalLayout_3);
 
@@ -237,7 +287,7 @@ public:
         scrollArea->setWidgetResizable(true);
         scrollAreaWidgetContents = new QWidget();
         scrollAreaWidgetContents->setObjectName(QString::fromUtf8("scrollAreaWidgetContents"));
-        scrollAreaWidgetContents->setGeometry(QRect(0, 0, 832, 614));
+        scrollAreaWidgetContents->setGeometry(QRect(0, 0, 832, 605));
         verticalLayout_4 = new QVBoxLayout(scrollAreaWidgetContents);
         verticalLayout_4->setSpacing(0);
         verticalLayout_4->setObjectName(QString::fromUtf8("verticalLayout_4"));
@@ -255,8 +305,8 @@ public:
         verticalLayout_3->addWidget(scrollArea);
 
         verticalLayout_3->setStretch(0, 1);
-        verticalLayout_3->setStretch(2, 1);
-        verticalLayout_3->setStretch(5, 10);
+        verticalLayout_3->setStretch(3, 1);
+        verticalLayout_3->setStretch(7, 10);
 
         retranslateUi(JuliaPkgManagement);
 
@@ -265,19 +315,46 @@ public:
 
     void retranslateUi(QWidget *JuliaPkgManagement)
     {
-        JuliaPkgManagement->setWindowTitle(QCoreApplication::translate("JuliaPkgManagement", "Pkg Management for Julia", nullptr));
+        JuliaPkgManagement->setWindowTitle(QCoreApplication::translate("JuliaPkgManagement", "  Pkg Management for Julia", nullptr));
+#if QT_CONFIG(tooltip)
+        label_julia->setToolTip(QCoreApplication::translate("JuliaPkgManagement", "click this will open the webset of julia", nullptr));
+#endif // QT_CONFIG(tooltip)
         label_julia->setText(QCoreApplication::translate("JuliaPkgManagement", "Julia: a High Performance and Dynamically Typed Language.", nullptr));
+#if QT_CONFIG(tooltip)
+        btn_one_click->setToolTip(QCoreApplication::translate("JuliaPkgManagement", "Install julia with jill(A third package of pip)", nullptr));
+#endif // QT_CONFIG(tooltip)
         btn_one_click->setText(QCoreApplication::translate("JuliaPkgManagement", "One-Click to Install", nullptr));
+#if QT_CONFIG(tooltip)
+        btn_auto_check->setToolTip(QCoreApplication::translate("JuliaPkgManagement", "Auto check the environment of julia.exe", nullptr));
+#endif // QT_CONFIG(tooltip)
         btn_auto_check->setText(QCoreApplication::translate("JuliaPkgManagement", "Auto Check(Recommend)", nullptr));
+#if QT_CONFIG(tooltip)
+        label_julia_hub->setToolTip(QCoreApplication::translate("JuliaPkgManagement", "The webset of julia packages", nullptr));
+#endif // QT_CONFIG(tooltip)
         label_julia_hub->setText(QCoreApplication::translate("JuliaPkgManagement", "Julia-Hub", nullptr));
         label_julia_com_en->setText(QCoreApplication::translate("JuliaPkgManagement", "Julia-Community-EN", nullptr));
         label_julia_com_cn->setText(QCoreApplication::translate("JuliaPkgManagement", "Julia-Community-CN", nullptr));
         label_5->setText(QCoreApplication::translate("JuliaPkgManagement", "Julia Executor:", nullptr));
-        btn_load->setText(QCoreApplication::translate("JuliaPkgManagement", "Load", nullptr));
-        btn_add->setText(QCoreApplication::translate("JuliaPkgManagement", "Add", nullptr));
-        btn_uninstall->setText(QCoreApplication::translate("JuliaPkgManagement", "Uninstall", nullptr));
-        btn_upgrade->setText(QCoreApplication::translate("JuliaPkgManagement", "Upgrade", nullptr));
-        btn_upgrade_all->setText(QCoreApplication::translate("JuliaPkgManagement", "Upgrade All", nullptr));
+#if QT_CONFIG(tooltip)
+        btn_load->setToolTip(QCoreApplication::translate("JuliaPkgManagement", "Please select the path of julia.exe", nullptr));
+#endif // QT_CONFIG(tooltip)
+        btn_load->setText(QString());
+#if QT_CONFIG(tooltip)
+        btn_add->setToolTip(QCoreApplication::translate("JuliaPkgManagement", "Install Package", nullptr));
+#endif // QT_CONFIG(tooltip)
+        btn_add->setText(QString());
+#if QT_CONFIG(tooltip)
+        btn_uninstall->setToolTip(QCoreApplication::translate("JuliaPkgManagement", "Delete the Selected Package", nullptr));
+#endif // QT_CONFIG(tooltip)
+        btn_uninstall->setText(QString());
+#if QT_CONFIG(tooltip)
+        btn_upgrade->setToolTip(QCoreApplication::translate("JuliaPkgManagement", "Upgrade the Selected Package", nullptr));
+#endif // QT_CONFIG(tooltip)
+        btn_upgrade->setText(QString());
+#if QT_CONFIG(tooltip)
+        btn_upgrade_all->setToolTip(QCoreApplication::translate("JuliaPkgManagement", "Upgrade All Packages", nullptr));
+#endif // QT_CONFIG(tooltip)
+        btn_upgrade_all->setText(QString());
     } // retranslateUi
 
 };
